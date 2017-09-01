9c5dd03a9d59a4fa138d720e3fc1e3cdd6a4f8e2
Steve Westenbroek <smwesten@usgs.gov>
2017-09-01 16:14:14 -0500

    Add SWB ver 1.0 files for Central Sands to provide a comparison.

---
+++ central_sands/jupyter/usgs_plot_defaults.py
@@ -0,0 +1,15 @@
+import matplotlib as mpl
+
+mpl.rc('font',size=12)
+mpl.rc('font',family='Univers 57 Condensed')
+
+mpl.rc('xtick.major', size=7)
+mpl.rc('ytick.major', size=7)
+
+mpl.rc('xtick.major', pad=8)
+mpl.rc('ytick.major', pad=6)
+
+mpl.rc('xtick', direction='in')
+mpl.rc('ytick', direction='in')
+mpl.rc('xtick', top=True)
+mpl.rc('ytick', right=True)
